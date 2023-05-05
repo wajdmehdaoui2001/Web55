2b266dca2b88327aeb8347f57c41dab3908c9ded
wajdmehdaoui2001 <118026901+wajdmehdaoui2001@users.noreply.github.com>
2023-05-05 01:48:47 +0100

    Add files via upload

---
+++ Web55/menu.js
@@ -0,0 +1,9 @@
+var menu_toggle = document.querySelector('.menu_toggle');
+var menu = document.querySelector('.menu');
+var menu_toggle_span = document.querySelector('.menu_toggle span');
+
+menu_toggle.onclick = function(){
+    menu_toggle.classList.toggle('active');
+    menu_toggle_span.classList.toggle('active');
+    menu.classList.toggle('responsive') ;
+}
